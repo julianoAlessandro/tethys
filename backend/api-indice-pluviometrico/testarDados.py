2f7d68d928fb179bff9e6b2d1dbbb84db0b92c3c
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2023-11-15 00:50:55 -0300

    Add files via upload

---
+++ backend/api-indice-pluviometrico/testarDados.py
@@ -0,0 +1,37 @@
+# Versão:Python 3.12.0
+# Autor:Juliano Alessandro dos Santos
+# Dia: 03/11/2023
+# Biblioteca: Requests utilizada para fazer a requisição de um serviço na Web
+# refinamento do Algoritimo criando classes e metodos
+# FrameWork FastAPI, responsável por criar uma URL,localmente no meu computador, estou exibindo na
+# web os meus dados
+
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+
+@app.get("/weather/{duracao},{latitude},{longitude}")  # decoration
+def Previsao(duracao: int, latitude: float, longitude: float):
+    try:
+        import CadastroUsuario as c
+
+        usuarios = c.CadastroUsuario()
+        valores = usuarios.realizarPrevisao(duracao, latitude, longitude)
+        return valores
+    except Exception as e:
+        return HTTPException(status_code=500, detail="Error %s" % str(e))
+
+
+@app.get("/weather/{latitude},{longitude}")
+def Indice(latitude: float, longitude: float):
+    try:
+        import CadastroUsuario as c
+
+        usuarios = c.CadastroUsuario()
+        valores = usuarios.BuscarPrecipitacao(latitude, longitude)
+
+        return valores
+
+    except Exception as e:
+        return HTTPException(status_code=500, detail="Error %s" % str(e))
