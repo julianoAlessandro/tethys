4923397b4163f9059f7ca7ab4ea4ea5b7ebdbd00
Gabriel Almir <gabr.almir@gmail.com>
2023-11-16 10:46:44 +0000

    refactor: reestructure api-indice-pluviometrico

--- backend/api-indice-pluviometrico/Classes/CadastroUsuario.py
+++ backend/api-indice-pluviometrico/Classes/CadastroUsuario.py
@@ -1,17 +1,18 @@
+import os
+import requests as r
+
+TOKEN = os.environ.get("WEATHER_TOKEN")
+
 class CadastroUsuario:
     def BuscarPrecipitacao(self, latitude, longitude):
-        import requests as r
-
-        url = f"https://atlas.microsoft.com/weather/currentConditions/json?api-version=1.0&query={latitude},{longitude}&subscription-key=g9cogBIB7upc32WNQMsBt-fsO5z2jpNurTF-r3mwVLQ"
+        url = f"https://atlas.microsoft.com/weather/currentConditions/json?api-version=1.0&query={latitude},{longitude}&subscription-key={TOKEN}"
         valores = r.get(url)
         formatoDicionario = valores.json()
         resumoPrecipitação = formatoDicionario["results"][0]["precipitationSummary"]
         return resumoPrecipitação
 
     def realizarPrevisao(self, duracao: int, latitude: float, longitude: float):
-        import requests as r
-
-        url = f"https://atlas.microsoft.com/weather/forecast/daily/json?api-version=1.0&query={latitude},{longitude}&duration={duracao}&subscription-key=g9cogBIB7upc32WNQMsBt-fsO5z2jpNurTF-r3mwVLQ"
+        url = f"https://atlas.microsoft.com/weather/forecast/daily/json?api-version=1.0&query={latitude},{longitude}&duration={duracao}&subscription-key={TOKEN}"
         valores = r.get(url)
         formatoDicionario = valores.json()
         valores = range(0, duracao)
